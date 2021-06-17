72c42103bdaad893fc1b988bccb54b09529427e0
Lavanbradley <lavan.bradley@bryanuniversity.edu>
2021-06-16 19:28:09 -0700

    Week 2

--- Week2/Finding and Testing APIs in Postman/app.js
+++ Week2/Finding and Testing APIs in Postman/app.js
@@ -1,33 +1,12 @@
+fetch('https://swapi.dev/api/people/').then(Response => Response.json()).then(characters => showCharacters(characters.results));
 
-var data = {
-  "name": "C-3PO",
-  "height": "167",
-  "mass": "75",
-  "hair_color": "n/a",
-  "skin_color": "gold",
-  "eye_color": "yellow",
-  "birth_year": "112BBY",
-  "gender": "n/a",
-  "homeworld": "http://swapi.dev/api/planets/1/",
-  "films": [
-    "http://swapi.dev/api/films/1/",
-    "http://swapi.dev/api/films/2/",
-    "http://swapi.dev/api/films/3/",
-    "http://swapi.dev/api/films/4/",
-    "http://swapi.dev/api/films/5/",
-    "http://swapi.dev/api/films/6/"
-  ],
-  "species": [
-    "http://swapi.dev/api/species/2/"
-  ],
-  "vehicles": [],
-  "starships": [],
-  "created": "2014-12-10T15:10:51.357000Z",
-  "edited": "2014-12-20T21:17:50.309000Z",
-  "url": "http://swapi.dev/api/people/2/"
-};
+showCharacters = characters => {
+  const charactersDiv = document.querySelector('.people');
+  characters.forEach(character => {
+    const characterElement = document.createElement('p');
+    var people = JSON.stringify(character)
+    characterElement.innerText = `${people}`
 
-console.log(data)
-var jsonData = JSON.stringify(data)
-document.body.innerHTML = jsonData;
-
+    charactersDiv.append(characterElement);
+  })
+}
